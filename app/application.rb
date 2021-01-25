f0104f38b365a5077c2a96c96d4eb7f798165e79
theo <teddyhoover@gmail.com>
2021-01-25 10:55:55 -0600

    commit tree 0824392be2949621dd57bfb102aff80c1360a0da

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,18 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
+    num_1 = Kernel.rand(1..10)
+    num_2 = Kernel.rand(1..10)
+    num_3 = Kernel.rand(1..10)
+
+    if num_1==num_2 && num_2==num_3
+      resp.write "#{num_1}, #{num_2}, #{num_3} You Win"
+    else
+      resp.write "#{num_1}, #{num_2}, #{num_3} You Lose"
+    end
+
     resp.finish
   end
 
-end
+end 
